9857b981fb322c2a706a7eb2ce72fa4014ea1e00
Yusuf YILDIZ <a.yusuf.yildiz@hotmail.com>
2020-08-26 18:06:48 +0300

    Initial Commit

--- App.js
+++ App.js
@@ -1,12 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Image360View from './components/360view';
+
+const remoteImages = [
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-1.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-2.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-3.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-4.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-5.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-6.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-7.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-8.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-9.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-10.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-11.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-12.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-13.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-14.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-15.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-16.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-17.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-18.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-19.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-20.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-21.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-22.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-23.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-24.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-25.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-26.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-27.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-28.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-29.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-30.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-31.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-32.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-33.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-34.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-35.jpg',
+  'https://raw.githubusercontent.com/phuochau/react-native-360-image-viewer/master/example/images/iris-36.jpg'
+].reverse();
+
+const localImages = [
+  require(`./images/iris-1.jpg`),
+  require(`./images/iris-2.jpg`),
+  require(`./images/iris-3.jpg`),
+  require(`./images/iris-4.jpg`),
+  require(`./images/iris-5.jpg`),
+  require(`./images/iris-6.jpg`),
+  require(`./images/iris-7.jpg`),
+  require(`./images/iris-8.jpg`),
+  require(`./images/iris-9.jpg`),
+  require(`./images/iris-10.jpg`),
+  require(`./images/iris-11.jpg`),
+  require(`./images/iris-12.jpg`),
+  require(`./images/iris-13.jpg`),
+  require(`./images/iris-14.jpg`),
+  require(`./images/iris-15.jpg`),
+  require(`./images/iris-16.jpg`),
+  require(`./images/iris-17.jpg`),
+  require(`./images/iris-18.jpg`),
+  require(`./images/iris-19.jpg`),
+  require(`./images/iris-20.jpg`),
+  require(`./images/iris-21.jpg`),
+  require(`./images/iris-22.jpg`),
+  require(`./images/iris-23.jpg`),
+  require(`./images/iris-24.jpg`),
+  require(`./images/iris-25.jpg`),
+  require(`./images/iris-26.jpg`),
+  require(`./images/iris-27.jpg`),
+  require(`./images/iris-28.jpg`),
+  require(`./images/iris-29.jpg`),
+  require(`./images/iris-30.jpg`),
+  require(`./images/iris-31.jpg`),
+  require(`./images/iris-32.jpg`),
+  require(`./images/iris-33.jpg`),
+  require(`./images/iris-34.jpg`),
+  require(`./images/iris-35.jpg`),
+  require(`./images/iris-36.jpg`)
+].reverse();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image360View
+        local
+        srcset={localImages}
+      />
     </View>
   );
 }
---
+++ components/360view/index.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { Dimensions, StyleSheet, Image } from 'react-native'
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+import Animated, {
+  add,
+  sub,
+  cond,
+  divide,
+  eq,
+  set,
+  useCode,
+  multiply,
+  floor,
+} from "react-native-reanimated";
+import {
+  usePanGestureHandler,
+  useValue,
+} from "react-native-redash";
+
+const { width } = Dimensions.get('window');
+
+const Image360View = ({ srcset, width, height, local }) => {
+  const startX = useValue(0);
+  const currentX = useValue(0);
+  const startRotation = useValue(0);
+  const rotation = useValue(0);
+  const index = useValue(0);
+  const rotationPeriod = useValue(360 / srcset.length);
+
+  const {
+    gestureHandler,
+    state,
+    position,
+  } = usePanGestureHandler();
+
+  useCode(() => [
+    cond(eq(state, State.BEGAN), [
+      set(startX, position.x),
+      set(startRotation, rotation),
+    ]),
+    cond(eq(state, State.ACTIVE), [
+      set(currentX, position.x),
+      set(rotation, add(startRotation, divide(multiply(sub(currentX, startX), 180), multiply(0.5, width)))),
+      set(index, floor(divide(sub(rotation, multiply(floor(divide(rotation, 360)), 360)), rotationPeriod))), // Math.floor((rotation - Math.floor(rotation / 360) * 360) / rotatePeriod)
+    ]),
+    cond(eq(state, State.END), [
+      set(currentX, position.x),
+      set(rotation, add(startRotation, divide(multiply(sub(currentX, startX), 180), multiply(0.5, width)))),
+      set(index, floor(divide(sub(rotation, multiply(floor(divide(rotation, 360)), 360)), rotationPeriod))), // Math.floor((rotation - Math.floor(rotation / 360) * 360) / rotatePeriod)
+    ]),
+  ], [width])
+
+  return (
+    <PanGestureHandler {...gestureHandler}>
+      <Animated.View style={{ width, height }}>
+        {srcset.map((img, i) => {
+          const isActive = eq(index, i);
+
+          return (
+            <Animated.Image
+              key={`${i}`}
+              source={local ? img : { uri: img }}
+              style={[
+                styles.image,
+                { width, height },
+                {
+                  opacity: cond(isActive, 1, 0),
+                }
+              ]}
+            />
+          )
+        })}
+      </Animated.View>
+    </PanGestureHandler>
+  )
+}
+
+const styles = StyleSheet.create({
+  image: {
+    position: 'absolute',
+    top: 0, left: 0,
+    resizeMode: 'cover',
+  },
+})
+
+Image360View.defaultProps = {
+  width,
+  height: 300,
+  srcset: [],
+  rotationRatio: 0.5,
+  local: false,
+}
+
+export default Image360View
